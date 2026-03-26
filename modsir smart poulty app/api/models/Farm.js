d8e18c94dbe8f0cb460f1089c60ac3abfc9b8959
ModSir29 <mordikay2910@gmail.com>
2026-03-26 10:06:32 +0200

    Delete modsir smart poulty app directory

--- modsir smart poulty app/api/models/Farm.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const farmSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Farm', farmSchema);
